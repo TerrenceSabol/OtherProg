df4f3d54d02bd20c2dd0a40ccfc1965806e0a792
TerrenceSabol <terry.sabol@newberry.edu>
2015-03-13 16:09:28 -0400

    ViewHousingPreference
    update to clear grids upon button click

---
+++ StudyHallAdmin/ViewHousingPreferences/ViewHousingPreferences/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView3 = New System.Windows.Forms.DataGridView()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(-2, 281)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(1448, 389)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(33, 24)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Assigned"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(33, 75)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Unassigned "
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(220, 74)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "All"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(522, 12)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(908, 85)
+        Me.DataGridView2.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(368, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Roommate Information"
+        '
+        'DataGridView3
+        '
+        Me.DataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView3.Location = New System.Drawing.Point(522, 103)
+        Me.DataGridView3.Name = "DataGridView3"
+        Me.DataGridView3.Size = New System.Drawing.Size(882, 150)
+        Me.DataGridView3.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(368, 103)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Room Preferences"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1474, 648)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DataGridView3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "View Room Selection"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents DataGridView2 As System.Windows.Forms.DataGridView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView3 As System.Windows.Forms.DataGridView
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ ViewHousingPreferences/ViewHousingPreferences/ViewHousingPreferences/Form1.vb
@@ -0,0 +1,165 @@
+﻿Imports System
+Imports System.Data
+
+Public Class Form1
+    Public x As String
+     
+    Private Function FetchDataEligible() As DataTable
+
+         
+
+        Using Conn As New Data.SqlClient.SqlConnection("Data Source=NCSQL1;Initial Catalog=tmseprd;timeout=60; User Id=sa;Password=jenzadmin;"), Command As New Data.SqlClient.SqlCommand("SELECT distinct nm.last_name, nm.first_name, nm.id_num,     b.EnableGroup,b.EnableDate, b.EnableTime, ns.BLDG_CDE, ns.ROOM_CDE as AssignedRoom, ns.ROOM_SLOT_NUM, ns.ASSIGN_DTE,full_status as EligibleStatus, cast(b.EnableSeq as Int) as Controlseq , b.ID     FROM dbo.nc_housing_select_ctl b left join dbo.STUD_ROOM_PREFS a on a.id_num=b.id and a.sess_cde =(select ctlfld1 from NC_Control_Info where ctlname='ReturnSESS') left join name_master nm on b.id=nm.id_num left join room_assign ns on b.id=ns.id_num and ns.sess_cde=(select ctlfld1 from NC_Control_Info where ctlname='ReturnSESS') where Full_status='Eligible'   ORDER BY   cast(b.EnableSeq as Int) , b.ID ", Conn), sAdapter As New Data.SqlClient.SqlDataAdapter(Command)
+            Try
+                Dim Table As New DataTable
+                sAdapter.Fill(Table)
+                Return Table
+            Catch o As Exception
+
+                MsgBox(o.Message.ToString)
+            End Try
+        End Using
+
+    End Function
+    Private Function FetchDataAssigned() As DataTable
+
+         
+        Using Conn As New Data.SqlClient.SqlConnection("Data Source=NCSQL1;Initial Catalog=tmseprd;timeout=60; User Id=sa;Password=jenzadmin;"), Command As New Data.SqlClient.SqlCommand("SELECT distinct nm.last_name, nm.first_name, nm.id_num,      b.EnableGroup,b.EnableDate, b.EnableTime, ns.BLDG_CDE, ns.ROOM_CDE as AssignedRoom, ns.ROOM_SLOT_NUM, ns.ASSIGN_DTE,full_status as EligibleStatus, cast(b.EnableSeq as Int) as Controlseq , b.ID   FROM dbo.nc_housing_select_ctl b left join dbo.STUD_ROOM_PREFS a on a.id_num=b.id and a.sess_cde =(select ctlfld1 from NC_Control_Info where ctlname='ReturnSESS') left join name_master nm on b.id=nm.id_num left join room_assign ns on b.id=ns.id_num and ns.sess_cde=(select ctlfld1 from NC_Control_Info where ctlname='ReturnSESS') where  ns.room_cde is not null ORDER BY  last_name,first_name, cast(b.EnableSeq as Int) , b.ID ", Conn), sAdapter As New Data.SqlClient.SqlDataAdapter(Command)
+            Try
+                Dim Table As New DataTable
+                sAdapter.Fill(Table)
+                Return Table
+            Catch o As Exception
+
+                MsgBox(o.Message.ToString)
+            End Try
+        End Using
+
+    End Function
+    Private Function FetchDataUnAssigned() As DataTable
+
+         
+        Using Conn As New Data.SqlClient.SqlConnection("Data Source=NCSQL1;Initial Catalog=tmseprd;timeout=60; User Id=sa;Password=jenzadmin;"), Command As New Data.SqlClient.SqlCommand("SELECT distinct nm.last_name, nm.first_name, nm.id_num,     b.EnableGroup,b.EnableDate, b.EnableTime, ns.BLDG_CDE, ns.ROOM_CDE as AssignedRoom, ns.ROOM_SLOT_NUM, ns.ASSIGN_DTE,full_status as EligibleStatus,  cast(b.EnableSeq as Int) as Controlseq , b.ID   as Controlseq FROM dbo.nc_housing_select_ctl b left join dbo.STUD_ROOM_PREFS a on a.id_num=b.id and a.sess_cde =(select ctlfld1 from NC_Control_Info where ctlname='ReturnSESS') left join name_master nm on b.id=nm.id_num left join room_assign ns on b.id=ns.id_num and ns.sess_cde=(select ctlfld1 from NC_Control_Info where ctlname='ReturnSESS') where  ns.room_cde is null and full_status ='Eligible'  ORDER BY   cast(b.EnableSeq as Int) , b.ID ", Conn), sAdapter As New Data.SqlClient.SqlDataAdapter(Command)
+            Try
+                Dim Table As New DataTable
+                sAdapter.Fill(Table)
+                Return Table
+            Catch o As Exception
+
+                MsgBox(o.Message.ToString)
+            End Try
+        End Using
+
+    End Function
+    Private Function FetchData() As DataTable
+
+        
+        Using Conn As New Data.SqlClient.SqlConnection("Data Source=NCSQL1;Initial Catalog=tmseprd;timeout=60; User Id=sa;Password=jenzadmin;"), Command As New Data.SqlClient.SqlCommand("SELECT distinct nm.last_name, nm.first_name, nm.id_num,     b.EnableGroup,b.EnableDate, b.EnableTime, ns.BLDG_CDE, ns.ROOM_CDE as AssignedRoom, ns.ROOM_SLOT_NUM, ns.ASSIGN_DTE,full_status as EligibleStatus, cast(b.EnableSeq as Int) as Controlseq , b.ID FROM dbo.nc_housing_select_ctl b left join dbo.STUD_ROOM_PREFS a on a.id_num=b.id and a.sess_cde =(select ctlfld1 from NC_Control_Info where ctlname='ReturnSESS') left join name_master nm on b.id=nm.id_num left join room_assign ns on b.id=ns.id_num and ns.sess_cde=(select ctlfld1 from NC_Control_Info where ctlname='ReturnSESS')     ORDER BY   cast(b.EnableSeq as Int) , b.ID   ", Conn), sAdapter As New Data.SqlClient.SqlDataAdapter(Command)
+            Try
+                Dim Table As New DataTable
+                sAdapter.Fill(Table)
+                Return Table
+            Catch o As Exception
+
+                MsgBox(o.Message.ToString)
+            End Try
+        End Using
+
+    End Function
+    Private Function FetchRoommateInfo() As DataTable
+
+
+        Using Conn As New Data.SqlClient.SqlConnection("Data Source=NCSQL1;Initial Catalog=tmseprd;timeout=60; User Id=sa;Password=jenzadmin;"), Command As New Data.SqlClient.SqlCommand("SELECT  case when Req_Actual_flag ='R' then 'Requested' when Req_Actual_flag ='A' Then 'Actual' else ' ' end as Request_Actual, nm.last_name + ',' + nm.first_name as Roommate_Name, Roommate_ID,Bldg_cde,Room_Cde,a.User_name as Created_UpdatedBy,a.Job_Name as Job,a.Job_Time   FROM dbo.Stud_roommates a left join name_master nm on a.roommate_id=nm.id_num   where a.sess_cde =(select ctlfld1 from NC_Control_Info where ctlname='ReturnSESS')  and a.id_num=" & x & "  ORDER BY   last_name,first_name,req_actual_flag", Conn), sAdapter As New Data.SqlClient.SqlDataAdapter(Command)
+            Try
+                Dim Table As New DataTable
+                sAdapter.Fill(Table)
+                Return Table
+            Catch o As Exception
+
+                MsgBox(o.Message.ToString)
+            End Try
+        End Using
+
+    End Function
+    Function FetchRoomPrefs() As DataTable
+        Using Conn As New Data.SqlClient.SqlConnection("Data Source=NCSQL1;Initial Catalog=tmseprd;timeout=60; User Id=sa;Password=jenzadmin;"), Command As New Data.SqlClient.SqlCommand(" SELECT ROOM_PREF_BLDG, ROOM_PREF_ROOM, ROOM_PREF_PTY, ROOM_CDE, ROOM_SLOT_NUM, ROOM_ASSIGN_STS FROM dbo.STUD_ROOM_PREFS a left join room_assign b on a.SESS_CDE=b.sess_cde and ROOM_PREF_BLDG= BLDG_CDE and ROOM_PREF_ROOM=b.room_cde WHERE a.SESS_CDE=(Select ctlfld1 from NC_control_info where ctlname='Returnsess') and a.id_num=" & x & "  ORDER BY Room_assign_sts,room_PREF_PTY   ", Conn), sAdapter As New Data.SqlClient.SqlDataAdapter(Command)
+            Try
+                Dim Table As New DataTable
+                sAdapter.Fill(Table)
+                Return Table
+            Catch o As Exception
+
+                MsgBox(o.Message.ToString)
+            End Try
+        End Using
+
+    End Function
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        DataGridView1.DataSource = FetchDataEligible()
+    End Sub
+
+    Private Sub DataGridView1_CellClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+
+        Dim cellroomstatus As String
+        x = DataGridView1.CurrentRow.Cells(2).Value.ToString()
+        DataGridView2.DataSource = "null"
+        DataGridView3.DataSource = "null"
+
+        DataGridView2.DataSource = FetchRoommateInfo()
+        DataGridView3.DataSource = FetchRoomPrefs()
+        If Me.DataGridView3.Rows.Count > 0 Then
+            For i As Integer = 0 To Me.DataGridView3.Rows.Count - 1
+                If IsDBNull(Me.DataGridView3.Rows(i).Cells(5).Value) = False Then
+                    If Me.DataGridView3.Rows(i).Cells(5).Value = "A" Then
+                        Me.DataGridView3.Rows(i).DefaultCellStyle.ForeColor = Color.Red
+                    End If
+                End If
+            Next
+
+        End If
+    End Sub
+
+     
+
+     
+      
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        DataGridView1.DataSource = FetchDataAssigned()
+        DataGridView2.DataSource = "null"
+        DataGridView3.DataSource = "null"
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        DataGridView1.DataSource = FetchDataUnAssigned()
+        DataGridView2.DataSource = "null"
+        DataGridView3.DataSource = "null"
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        DataGridView1.DataSource = FetchData()
+        DataGridView2.DataSource = "null"
+        DataGridView3.DataSource = "null"
+
+
+
+        For i As Integer = 0 To Me.DataGridView1.Rows.Count - 1
+            If Me.DataGridView1.Rows(i).Cells(10).Value = "Not Yet Eligible" Then
+                Me.DataGridView1.Rows(i).Cells(10).Style.ForeColor = Color.Red
+            Else
+                Me.DataGridView1.Rows(i).Cells(10).Style.ForeColor = Color.Green
+            End If
+        Next
+
+
+
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
